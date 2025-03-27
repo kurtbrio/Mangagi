cf02d537f42c66aa7838e0e6241dc2f8174cb9dd
Kurt Briones <kurtbriodev@gmail.com>
2025-03-27 18:53:07 +0800

    Fixing styles

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,26 +11,34 @@ const Navbar = () => {
   const [isFocused, setIsFocused] = useState(false);
 
   return (
-    <nav className="fixed w-screen h-fit px-2.5 py-5 flex items-center justify-around gap-5 bg-none z-999">
-      <h1 className="!text-4xl font-bold">Mangagi</h1>
+    <nav className="fixed w-screen h-fit px-2.5 py-2 flex items-center justify-around gap-5 bg-none z-999">
+      <h1
+        className={`text-4xl font-bold ${
+          theme === "light" ? "!text-[#3a3951]" : "!text-white"
+        } `}
+      >
+        Mangagi
+      </h1>
       <div className="hidden sm:flex items-center justify-between w-[80%]">
         <form className="flex items-center relative">
           <input
             type="text"
             placeholder="Search manga..."
             value={q}
-            className="min-w-72  h-12 text-gray-500 !text-xl p-5"
+            className="min-w-72 h-10 bg-white font-medium !text-md p-5"
             onChange={(e) =>
               dispatch({ type: "SET_QUERY", payload: e.target.value })
             }
             onFocus={() => setIsFocused(true)}
             onBlur={() => setTimeout(() => setIsFocused(false), 200)}
           />
-          <p className="absolute text-xl top-0 right-5 translate-x-1/2 translate-y-1/2">
-            <a href="/filter">
-              <i class="bx bxs-filter-alt"></i>
-            </a>
-          </p>
+
+          <a
+            href="/filter"
+            className="absolute text-xl top-1/2 right-5 translate-x-1/2 -translate-y-1/2 !text-[#3a3951]"
+          >
+            <i class="bx bxs-filter-alt"></i>
+          </a>
 
           {/* display search result */}
           {isFocused && q.length > 2 && (
@@ -39,15 +47,15 @@ const Navbar = () => {
         </form>
 
         <ul className="flex gap-2">
-          <li className="text-white bg-blue-400 rounded-full ">
+          <li className="bg-blue-400 rounded-full">
             <a
               href="https://x.com/kurtbrio"
-              className="w-8 h-8 text-xl flex items-center justify-center"
+              className="w-8 h-8 text-xl flex items-center justify-center "
             >
               <i class="bx bxl-twitter"></i>
             </a>
           </li>
-          <li className="text-white bg-black rounded-full">
+          <li className="bg-black rounded-full">
             <a
               href="https://github.com/kurtbrio"
               className="w-8 h-8 text-xl flex items-center justify-center"
@@ -55,7 +63,7 @@ const Navbar = () => {
               <i class="bx bxl-github"></i>
             </a>
           </li>
-          <li className="text-white bg-[#2A475E] rounded-full">
+          <li className="bg-[#2A475E] rounded-full">
             <a
               href="https://steamcommunity.com/profiles/76561199133854221/"
               className="w-8 h-8 text-xl flex items-center justify-center"
@@ -64,15 +72,18 @@ const Navbar = () => {
             </a>
           </li>
         </ul>
-        <ul className="flex gap-5">
+        <ul className="flex gap-5 text-center">
           <li className="text-2xl">
-            <button>
+            <button className="!text-[#ffbade] !bg-transparent">
               <i class="bx bx-scatter-chart"></i>
               <p className="text-sm">Random</p>
             </button>
           </li>
           <li className="text-2xl">
-            <button onClick={toggleTheme}>
+            <button
+              onClick={toggleTheme}
+              className="!text-[#ffbade] !bg-transparent"
+            >
               {theme === "dark" ? (
                 <i class="bx bxs-moon"></i>
               ) : theme === "light" ? (
@@ -84,7 +95,9 @@ const Navbar = () => {
             </button>
           </li>
         </ul>
-        <button className="w-20 h-10 bg-red-50">Login</button>
+        <button className="rounded-md font-bold w-20 h-10 bg-red-50 ">
+          Login
+        </button>
       </div>
     </nav>
   );
--- src/pages/Home/Spotlight.jsx
+++ src/pages/Home/Spotlight.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { SpotlightContext } from "../../context/SpotlightContext";
 import Loader from "../../components/Loader";
 
@@ -9,49 +9,71 @@ const Spotlight = () => {
   const spotlightManga = results.slice(0, 8);
   const [currentIndex, setCurrentIndex] = useState(0);
 
-  const carousel = () => {
-    setCurrentIndex((prev) => (prev + 1) % spotlightManga.length);
+  useEffect(() => {
+    if (spotlightManga.length === 0) return;
+
+    const interval = setInterval(() => {
+      setCurrentIndex((prev) => (prev + 1) % spotlightManga.length);
+    }, [5000]);
+    return () => clearTimeout(interval);
+  }, [spotlightManga.length, currentIndex]);
+
+  const handleNext = () => {
+    return setCurrentIndex((prev) => (prev + 1) % spotlightManga.length);
   };
 
-  console.log(spotlightManga);
+  const handlePrev = () => {
+    return setCurrentIndex(
+      (prev) => (prev - 1 + spotlightManga.length) % spotlightManga.length
+    );
+  };
 
   return (
     <section className="w-screen h-screen flex items-center relative">
       {spotlightManga.length > 0 ? (
         <>
-          <div className="ml-10 absolute text-white left-0 w-1/2 z-10 space-y-5">
-            <p className="text-xl">#{currentIndex + 1} Spotlight</p>
-            <h1 className="text-5xl">{spotlightManga[currentIndex].title}</h1>
-            <div className="flex flex-wrap gap-5">
-              {spotlightManga[currentIndex].genres.map((theme) => (
-                <p className="py-2 px-4 bg-red-500 rounded-full text-xs whitespace-nowrap">
+          <div className="ml-10 absolute left-0 w-6/12 z-10 space-y-5">
+            <h1 className="text-xl">#{currentIndex + 1} Spotlight</h1>
+            <p className="text-5xl">{spotlightManga[currentIndex].title}</p>
+
+            <div className="hidden md:flex flex-wrap gap-5">
+              {spotlightManga[currentIndex].genres.map((theme, index) => (
+                <p
+                  key={index}
+                  className="py-2 px-4 bg-[#ffbade] text-[#3a3951] font-bold rounded-full text-xs whitespace-nowrap"
+                >
                   {theme.name}
                 </p>
               ))}
+              <p className="text-lg line-clamp-3">
+                {spotlightManga[currentIndex].synopsis}
+              </p>
             </div>
-            <p className="text-lg line-clamp-3">
-              {spotlightManga[currentIndex].synopsis}
-            </p>
-            <a className="py-2 px-4 bg-red-500 text-lg rounded-full" href="">
+            <a className="py-2 px-4 bg-[#3a3951] text-lg rounded-full" href="">
               Detail <i class="bx bxs-chevron-right"></i>
             </a>
           </div>
-          <div className="w-full h-full  overflow-hidden relative">
+          <div className="w-full h-full  overflow-hidden relative ">
             <img
-              src={spotlightManga[currentIndex]?.images?.jpg?.large_image_url}
+              src={spotlightManga[currentIndex]?.images?.webp.large_image_url}
               alt={spotlightManga[currentIndex]?.title}
-              className="absolute h-full shadow-2xl shadow-black right-0 w-1/2 object-top object-cover"
+              className="absolute h-full right-4 w-full lg:w-1/2 object-top object-cover"
             />
 
-            <div className="absolute inset-0 bg-gradient-to-b from-black/60 via-white/1 to-transparent"></div>
-            <div className="absolute inset-0 bg-gradient-to-r from-black via-white/1 to-transparent"></div>
+            <div className="absolute inset-0 bg-black/40 bg-gradient-to-b from-[#201f31]/80 via-transparent to-[#201f31]/80"></div>
           </div>
-          <div className="absolute right-0">
+          <div className="absolute flex flex-col right-[1%] bottom-[10%] text-3xl">
             <button
-              onClick={carousel}
-              className="mt-4 px-4 py-2 bg-blue-500 text-white rounded-lg"
+              onClick={handleNext}
+              className="mt-4 w-10 h-10 bg-[#3a3951] text-white rounded-lg"
             >
-              Next
+              <i class="bx bx-chevron-right"></i>
+            </button>
+            <button
+              onClick={handlePrev}
+              className="mt-4 w-10 h-10 bg-[#3a3951] text-white rounded-lg"
+            >
+              <i class="bx bx-chevron-left"></i>
             </button>
           </div>
         </>
